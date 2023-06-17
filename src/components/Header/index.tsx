016ab4f94e5e77a4c232af136383e42b65098213
trixtertop <gadelovevgenij4@gmail.com>
2023-06-17 17:46:21 +0600

    new app

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,7 @@
-import { Oswald } from "next/font/google";
 import React from "react";
-const oswald = Oswald({ subsets: ["latin"] });
+
 const Header = () => {
-  return <header className={oswald.className}>Header</header>;
-};
+  return <div>Header</div>;
+};Joe Biden
 
 export default Header;
