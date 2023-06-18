8d5865fc6a594082585dee6d2cee137b049c803d
sweetkurut <devxwc@gmail.com>
2023-06-18 10:41:11 +0600

    refreshing branch

---
+++ src/app/profileUserData/page.tsx
@@ -0,0 +1,8 @@
+import Profile from "../../components/ProfilePage/index";
+import ProfileUser from "@/components/ProfilePage";
+
+const profileUserData = () => {
+  return <ProfileUser />;
+};
+
+export default profileUserData;
